14db7406831e0347ff935de748fd50e3f963efc7
Maranici <cuestamatteo@gmail.com>
2024-06-23 17:33:29 +0200

    Correction on the ft_sort_int_tab : now the function returns to 0 after executing a swap. This solves a lot of problems.

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: macuesta <macuesta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 17:27:52 by macuesta          #+#    #+#             */
-/*   Updated: 2024/06/21 17:36:55 by macuesta         ###   ########.fr       */
+/*   Updated: 2024/06/23 17:32:12 by macuesta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,18 @@ void	ft_sort_int_tab(int *tab, int size)
 			temp = tab[i];
 			tab[i] = tab[i + 1];
 			tab[i + 1] = temp;
+			i = 0;
 		}
-		i++;
+		else
+			i++;
 	}
 }
 
 // #include "stdio.h"
 // int	main(void)
 // {
-// 	int tab[10] = {0,1,2,3,32,5,6,7,65,64};
+// 	int tab[4] = {2,1,5,1};
 
-// 	ft_sort_int_tab(tab, 10);
-// 	printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5], tab[6], tab[7], tab[8], tab[9]);
+// 	ft_sort_int_tab(tab, 4);
+// 	printf("%d,%d,%d,%d", tab[0], tab[1], tab[2], tab[3]);
 // }
